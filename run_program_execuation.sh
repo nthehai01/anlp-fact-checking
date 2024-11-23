295f8e091e5c9cd9740d516c56442f9607d738f1
The-Hai Nguyen <nthehai01@gmail.com>
2024-11-23 13:28:56 +0900

    comment for program execution

--- run_program_execuation.sh
+++ run_program_execuation.sh
@@ -1,5 +1,5 @@
 python program_execution.py\
     --reasoning_program_path results/programs/HOVER-4_N=1_meta-llama/Llama-3.1-8B-Instruct_programs_v2.json\
     --dataset_path dev.json\
-    --output_path outputs/llama_3.1_8b_instruct_flan_t5_large.json\
+    --output_path results/executions/llama_3.1_8b_instruct_flan_t5_large.json\
     --QA_model_name google/flan-t5-large
